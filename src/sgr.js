dcf8e33ef8ed5563333c5680f54f6b3e3e475c18
Mathieu Breton <mathieu.breton@js-republic.com>
2016-11-24 01:16:48 +0100

    Add sgr init acceptance test

--- src/sgr.js
+++ src/sgr.js
@@ -1,13 +1,34 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const fs = require('fs');
+const path = require('path');
 const packageJson = require('../package.json');
 const CLIForm = require('./CLIForm');
 
 program
-  .version(packageJson.version)
-  .parse(process.argv);
+  .description('Component showcase generator')
+  .version(packageJson.version);
 
-const cliForm = new CLIForm();
+program
+  .command('init')
+  .description('initialize configuration')
+  .action(() => {
+    const cliForm = new CLIForm();
+    cliForm.askQuestions((configuration) => {
+      const packageToUpdatePath = path.join(process.cwd(), 'package.json');
+      const packageToUpdate= JSON.parse(fs.readFileSync(packageToUpdatePath));
+      packageToUpdate.sgrConfig = configuration;
+      fs.writeFileSync(packageToUpdatePath, JSON.stringify(packageToUpdate, null, '  '));
+      process.exit();
+    });
+  });
 
-cliForm.askQuestions(() => process.exit());
+program
+  .command('run')
+  .description('generate the documentation')
+  .action(() => {
+    throw new Error('Not yet implemented')
+  });
+
+program.parse(process.argv);
