740ef2c5280ea6cf69698e1fc36339ed7763b4ab
VincentCordobes <vincent.cordobes@gmail.com>
2016-11-27 14:32:04 +0100

    Add cli questions

---
+++ src/cli/init.js
@@ -0,0 +1,47 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'inputDir',
+    message: 'Project directory: ',
+    default: '.\/',
+  },
+  {
+    type: 'input',
+    name: 'outputDir',
+    message: 'Documentation output: ',
+    default: '.\/component-docs',
+  },
+  {
+    type: 'input',
+    name: 'patternToIgnore',
+    message: 'Pattern to ignore: ',
+    default: './node_modules/*',
+  },
+  {
+    type: 'checkbox',
+    name: 'componentsType',
+    message: 'King of component: ',
+    choices: ['react', 'angular2', 'vuejs', 'polymer'],
+    default: ['react'],
+    validate: (answer) => {
+      if (answer.length < 1) {
+        return 'You must choose one';
+      }
+      return true;
+    },
+  },
+  {
+    type: 'input',
+    name: 'cssPath',
+    message: 'Additional CSS file: ',
+    default: '',
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  // Use user feedback for... whatever!!
+  console.log(`${chalk.green('✨ Congratulatio !')} you can find the generated documentation in ${chalk.red(answers.outputDir)}`);
+});
